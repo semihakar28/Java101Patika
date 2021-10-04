42262b410bd23032a8afd2d683fe8e4ed08096ea
Semih Akar <semihakar28@gmail.com>
2021-10-04 19:49:31 +0300

    Create notOrtalaması.java

---
+++ notOrtalaması.java
@@ -0,0 +1,40 @@
+package NotOrtalaması;
+
+import java.util.Scanner;
+
+public class NotOrtalamasi {
+    public static void main(String[] args) {
+        Scanner inp = new Scanner(System.in);
+        // Değişkenler
+        int mat,tur,fiz,kim,biy;
+        int mata = 5,tura=3,fiza=5,kima=3,biya=3;
+
+        // Notları alma
+        System.out.print("Matematik sınav notunuzu giriniz:");
+        mat = inp.nextInt();
+
+        System.out.print("Türkçe sınav notunuzu giriniz:");
+        tur = inp.nextInt();
+
+        System.out.print("Fizik sınav notunuzu giriniz:");
+        fiz = inp.nextInt();
+
+        System.out.print("Kimya sınav notunuzu giriniz:");
+        kim = inp.nextInt();
+
+        System.out.print("Biyoloji sınav notunuzu giriniz:");
+        biy = inp.nextInt();
+
+        // Hesaplama
+        double orthesapla = ((mat*mata)+(tur*tura)+(fiz*fiza)+(kim*kima)+(biy*biya));
+        int kstoplam = (mata+tura+fiza+kima+biya);
+        double ortalama = (orthesapla/kstoplam);
+
+        System.out.print("Not Ortalamanız=\t"+ortalama);
+
+
+
+
+    }
+
+}
